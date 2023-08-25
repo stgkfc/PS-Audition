fbec3c602eb3bf7b71a6e6adf477c82a91a849b6
stgkfc <143193505+stgkfc@users.noreply.github.com>
2023-08-25 08:25:15 -0700

    Create console.sh

---
+++ console.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#Install Programs
+sudo apt install -y wireshark
+
+#Run Commands/Scripts
+#Need to change to the Downloads directory and download the file. It is also availabe in the Repository.
+cd ~/Downloads
+wget https://pentestinglabs.com/Capture.pcapng
